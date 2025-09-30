7ec6b55ad38541f70fd97fbe47db18362f7fe1a8
Thomas David <tom@mightysmile.com>
2025-09-30 16:24:46 +0200

    refactor(core): rename deepl-translate-api.php to deepl-translate-rest-api.php and update include path

--- wp-deepl-translator.php
+++ wp-deepl-translator.php
@@ -29,5 +29,5 @@ if ( file_exists( DEEPL_TRANSLATE_PLUGIN_DIR . '/vendor/autoload.php' ) ) {
 
 // Include the main plugin files.
 require_once DEEPL_TRANSLATE_PLUGIN_DIR . 'includes/admin-deepl-translate.php';
-require_once DEEPL_TRANSLATE_PLUGIN_DIR . 'includes/deepl-translate-api.php';
+require_once DEEPL_TRANSLATE_PLUGIN_DIR . 'includes/deepl-translate-rest-api.php';
 require_once DEEPL_TRANSLATE_PLUGIN_DIR . 'includes/deepl-translate.php';
